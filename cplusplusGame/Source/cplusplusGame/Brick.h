c5bac9eec93773693df1ac9affa8c2db7a4f07aa
Iwan <iwanbuchler02@gmail.com>
2020-10-13 10:59:50 +0200

    Movement test in C++
    Paddle Ball game clone to test movement setup in C++ (UE4)

---
+++ cplusplusGame/Source/cplusplusGame/Brick.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Engine.h"
+
+#include "Ball.h"
+
+#include "Brick.generated.h"
+
+
+class  UBoxComponent;
+
+UCLASS()
+class CPLUSPLUSGAME_API ABrick : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABrick();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+    
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+        UStaticMeshComponent* SM_Brick;
+    
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    UBoxComponent* Box_Collision;
+    
+    
+    float SpeedModifierOnBounce = 0.01f;
+    
+    UFUNCTION()
+        void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndexType, bool bFromSweep, const FHitResult & SweepResult);
+    
+        void DestroyBrick();
+    
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
